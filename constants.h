05f9243dd06505a5fced39d9e083a0098b6c5574
AbdullahKhawaja786 <abdullahakhawaja786@gmail.com>
2025-12-02 13:36:57 +0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,35 +1,35 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 // Window settings
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 const char WINDOW_TITLE[] = "Brick Breaker Game";
 // Paddle settings
-const float PADDLE_WIDTH = 100.0f;
+const float PADDLE_WIDTH = 120.0f;
 const float PADDLE_HEIGHT = 20.0f;
-const float PADDLE_SPEED = 400.0f;
-const float PADDLE_Y = 550.0f;
+const float PADDLE_SPEED = 500.0f;
+const float PADDLE_Y = 670.0f;
 // Ball settings
-const float BALL_RADIUS = 8.0f;
-const float BALL_SPEED = 300.0f;
+const float BALL_RADIUS = 10.0f;
+const float BALL_SPEED = 350.0f;
 const float BALL_SPEED_INCREMENT = 50.0f;
 // Brick settings
-const int GRID_WIDTH = 10;
+const int GRID_WIDTH = 12;
 const int GRID_HEIGHT = 8;
 const int TOTAL_BRICKS = GRID_WIDTH * GRID_HEIGHT;
-const float BRICK_WIDTH = 75.0f;
-const float BRICK_HEIGHT = 25.0f;
-const float BRICK_OFFSET_X = 2.0f;
-const float BRICK_OFFSET_Y = 50.0f;
+const float BRICK_WIDTH = 100.0f;
+const float BRICK_HEIGHT = 30.0f;
+const float BRICK_OFFSET_X = 10.0f;
+const float BRICK_OFFSET_Y = 80.0f;
 const float BRICK_SPACING = 5.0f;
 // Power-up settings
 const int MAX_POWERUPS = 10;
-const float POWERUP_SIZE = 20.0f;
-const float POWERUP_FALL_SPEED = 100.0f;
+const float POWERUP_SIZE = 25.0f;
+const float POWERUP_FALL_SPEED = 120.0f;
 const int POWERUP_CHANCE = 20;
 // Particle settings
-const int MAX_PARTICLES = 100;
-const float PARTICLE_LIFETIME = 1.0f;
+const int MAX_PARTICLES = 50;
+const float PARTICLE_LIFETIME = 0.8f;
 // Background animation
 const int MAX_BG_STARS = 80;
 // Game states
@@ -49,9 +49,8 @@ const int MENU_OPTIONS_COUNT = 5;
 // Pause menu options
 const int PAUSE_RESUME = 0;
 const int PAUSE_SAVE = 1;
-const int PAUSE_SETTINGS = 2;
-const int PAUSE_MAIN_MENU = 3;
-const int PAUSE_OPTIONS_COUNT = 4;
+const int PAUSE_MAIN_MENU = 2;
+const int PAUSE_OPTIONS_COUNT = 3;
 // Power-up types
 const int POWERUP_NONE = 0;
 const int POWERUP_MULTIBALL = 1;
